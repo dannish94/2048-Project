fd11a6c0f8a0752c17f41b24f690d728fe3eb52d
Dannish Hussain <Dannishh94@gmail.com>
2016-04-15 20:56:40 +0100

    broken ai

--- src/aisolver/MinimaxAI.java
+++ src/aisolver/MinimaxAI.java
@@ -1,5 +1,14 @@
 package aisolver;
 
+import java.util.Map;
+
+import Controller.Directions;
+import model.Game;
+
+//Import enumerator for players
+
 public class MinimaxAI {
 
+	public static Directions nextBestMove(Game game, int depthnumber) throws Exception { 
+	Map<String, Object> output = Minimax(game, depthnumber, Players.Player_One);
 }
---
+++ src/aisolver/Players.java
@@ -0,0 +1,7 @@
+package aisolver;
+//Requires two players to play the game. One to play the game, other to perform heuristics
+public enum Players {
+
+	Player_One,
+	Player_Two
+}
